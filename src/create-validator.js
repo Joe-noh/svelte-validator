aed9b5d570cf9f74c37682efa400804bd9816daa
Joe-noh <goflb.jh@gmail.com>
2020-01-03 14:18:54 +0900

    immediate option

--- src/create-validator.js
+++ src/create-validator.js
@@ -1,16 +1,24 @@
 import { writable, derived } from 'svelte/store'
 
-export function createValidator({ initial, rules }) {
+export function createValidator(opts) {
+  const { initial, rules } = opts
   const validator = createValidatorFun(rules)
 
+  const configStore = writable(config(opts))
   const valueStore = writable(initial)
-  const errorStore = derived(valueStore, (value) => validator(value))
+  const errorStore = derived([valueStore, configStore], ([value, config]) => validator(value, config))
 
-  return [valueStore, errorStore]
+  const activate = () => {
+    configStore.update(config => ({ ...config, active: true }))
+  }
+
+  return [valueStore, errorStore, { activate }]
 }
 
 function createValidatorFun(rules) {
-  return (value) => {
+  return (value, config) => {
+    if (!config.active) { return {} }
+
     return rules.reduce((violated, rule) => {
       const { name, isValid, argument } = rule
 
@@ -21,4 +29,14 @@ function createValidatorFun(rules) {
       }
     }, {})
   }
+}
+
+function config(opts) {
+  return {
+    active: fetch(opts, 'immediate', true)
+  }
+}
+
+function fetch(opts, key, fallback) {
+  return key in opts ? opts[key] : fallback
 }
