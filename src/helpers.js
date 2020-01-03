77086616f9210c43098ff80db2c430bf6b09c18f
Joe-noh <goflb.jh@gmail.com>
2020-01-04 03:15:36 +0900

    Add getErrorOptions helper

--- src/helpers.js
+++ src/helpers.js
@@ -1,3 +1,14 @@
 export function hasError(errorObject) {
   return Object.keys(errorObject).length > 0
 }
+
+export function getErrorOptions(errorObject, errorNames, key = null) {
+  return errorNames.reduce((acc, name) => {
+    if (name in errorObject) {
+      const value = key ? errorObject[name][key] : errorObject[name]
+      return [...acc, value]
+    } else {
+      return acc
+    }
+  }, [])
+}
--- test/helpers.test.js
+++ test/helpers.test.js
@@ -1,8 +1,35 @@
-import { hasError } from '../src/index'
+import { hasError, getErrorOptions } from '../src/index'
 
 describe('hasError', () => {
   test('returns true if given object is not empty', () => {
     expect(hasError({})).toEqual(false)
     expect(hasError({ required: undefined })).toEqual(true)
+  })
+})
+
+describe('getErrorOptions', () => {
+  test('returns array of options', () => {
+    const errors = {
+      required: { message: 'Can not be blank' },
+      minLength: { message: 'Too short.' },
+    }
+
+    const options = getErrorOptions(errors, ['required', 'minLength'])
+    expect(options[0]).toEqual(errors.required)
+    expect(options[1]).toEqual(errors.minLength)
+    expect(options.length).toEqual(Object.keys(errors).length)
+  })
+
+  test('dig options if key is given', () => {
+    const errors = {
+      required: { message: 'Can not be blank' },
+      minLength: { message: 'Too short.' },
+    }
+
+    const messages = getErrorOptions(errors, ['required', 'minLength'], 'message')
+    expect(messages[0]).toEqual(errors.required.message)
+    expect(messages[1]).toEqual(errors.minLength.message)
+
+    expect(messages.length).toEqual(Object.keys(errors).length)
   })
 })
